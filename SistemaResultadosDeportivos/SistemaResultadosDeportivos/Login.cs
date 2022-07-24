c76ab4a9f7cd1b778b5c4e30aac8008c4e5d3570
Facundo Camargo <facundo.camargo.05@gmail.com>
2022-07-24 17:43:38 -0300

    APIs incorporadas (sin fallos)

--- SistemaResultadosDeportivos/SistemaResultadosDeportivos/Login.cs
+++ SistemaResultadosDeportivos/SistemaResultadosDeportivos/Login.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using SistemaResultadosDeportivos.Logica;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
